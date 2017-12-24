a6e6c3aaa420519981220b8cf5ef590ce1c292a9
Sem Schilder <sem@tropical.email>
2017-12-24 12:38:34 +0100

    Properly use getters and setters and annotate functions and variables

--- src/xvilo/Movieskoop/Movie.php
+++ src/xvilo/Movieskoop/Movie.php
@@ -7,21 +7,57 @@ class Movie
     /** @var string */
     private $id = '';
 
+    /** @var string  */
+    private $movieUrl = '';
+
     /**
      * Movie constructor.
      * @param string $id
      */
     public function __construct(string $id)
     {
-        $this->id = $id;
+        $this->setId($id);
+        $this->setMovieUrl('http://movieskoop.nl' . $this->getId());
         $this->populateObject();
     }
 
     private function populateObject()
     {
-        $pageBodyStream   = Util::getPageBodyFromUrl('http://movieskoop.nl' . $this->id);
+        $pageBodyStream   = Util::getPageBodyFromUrl($this->getMovieUrl());
         $pageBodyContents = $pageBodyStream->getContents();
 
         die(var_dump($pageBodyContents));
+    }
+
+    /**
+     * @param string $id
+     */
+    private function setId(string $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    private function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $movieUrl
+     */
+    private function setMovieUrl(string $movieUrl)
+    {
+        $this->movieUrl = $movieUrl;
+    }
+
+    /**
+     * @return string
+     */
+    private function getMovieUrl()
+    {
+        return $this->movieUrl;
     }
 }
