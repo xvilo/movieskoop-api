c34a4b3c959ba9d871a38756b9c16f9597ef0112
Sem Schilder <sem@tropical.email>
2017-12-24 12:35:27 +0100

    Basic set of movie ID and get page body data to extract movie data from

--- src/xvilo/Movieskoop/Movie.php
+++ src/xvilo/Movieskoop/Movie.php
@@ -4,8 +4,24 @@ namespace xvilo\Movieskoop;
 
 class Movie
 {
-    public function __construct($options)
+    /** @var string */
+    private $id = '';
+
+    /**
+     * Movie constructor.
+     * @param string $id
+     */
+    public function __construct(string $id)
     {
-        
+        $this->id = $id;
+        $this->populateObject();
+    }
+
+    private function populateObject()
+    {
+        $pageBodyStream   = Util::getPageBodyFromUrl('http://movieskoop.nl' . $this->id);
+        $pageBodyContents = $pageBodyStream->getContents();
+
+        die(var_dump($pageBodyContents));
     }
 }
